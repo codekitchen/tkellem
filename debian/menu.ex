e8873805076d513dd3008323de5d53d6a84511fd
paul cannon <paul@riptano.com>
2010-12-16 12:41:05 -0600

    first pass at packaging

--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(tkellem):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="tkellem" command="/usr/bin/tkellem"
--- debian/tkellem.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the tkellem package
-#
-0 4	* * *	root	[ -x /usr/bin/tkellem_maintenance ] && /usr/bin/tkellem_maintenance
