5f3c55ab3861789e079ee3df17772721cabba272
paul cannon <paul@riptano.com>
2010-12-16 11:58:55 -0600

    bare dh_make output
    from dh_make -c bsd -s -i -r

---
+++ debian/menu.ex
@@ -0,0 +1,2 @@
+?package(tkellem):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
+  title="tkellem" command="/usr/bin/tkellem"
---
+++ debian/tkellem.default.ex
@@ -0,0 +1,10 @@
+# Defaults for tkellem initscript
+# sourced by /etc/init.d/tkellem
+# installed at /etc/default/tkellem by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
