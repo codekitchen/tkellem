0e9fc8f7acf1e1032375219f2589bd69cee16afd
Brian Palmer <brianp@instructure.com>
2011-06-16 13:11:23 -0600

    fix specs

--- spec/irc_server_spec.rb
+++ spec/irc_server_spec.rb
@@ -3,26 +3,27 @@ require 'tkellem/irc_server'
 
 include Tkellem
 
-describe IrcServer, "connection" do
+describe Bouncer, "connection" do
   def make_server
-    Class.new do
-      include IrcServer
-
-      def send_data(*a); end
-    end.new(nil, "spec_server", false, "speccer")
+    EM.stub!(:add_timer).and_return(nil)
+    b = Bouncer.new(NetworkUser.new(:user => User.new(:username => 'speccer'), :network => Network.new))
+    b
   end
 
   def send_welcome(s, &just_before_last)
-    s.receive_line("001 blah blah")
-    s.receive_line("002 more blah")
-    s.receive_line("003 even more blah")
+    s.should_receive(:send_msg).with("USER speccer localhost blah :speccer")
+    s.should_receive(:send_msg).with("NICK speccer")
+    s.should_receive(:send_msg).with("AWAY :Away")
+    s.connection_established
+    s.server_msg(IrcMessage.parse("001 blah blah"))
+    s.server_msg(IrcMessage.parse("002 more blah"))
+    s.server_msg(IrcMessage.parse("003 even more blah"))
     just_before_last && just_before_last.call
-    s.receive_line("376 :end of MOTD")
+    s.server_msg(IrcMessage.parse("376 :end of MOTD"))
   end
 
   def connected_server
     s = make_server
-    s.post_init
     send_welcome(s)
     s.connected?.should be_true
     s
@@ -31,30 +32,15 @@ describe IrcServer, "connection" do
   it "should connect to the server on creation" do
     s = make_server
     s.connected?.should_not be_true
-    s.should_receive(:send_data).with("USER speccer localhost blah :speccer\r\n")
-    s.should_receive(:send_data).with("NICK speccer\r\n")
-    s.post_init
-  end
-
-  it "should join pending rooms once the connection is established" do
-    s = make_server
-    s.post_init
-    s.connected?.should_not be_true # still haven't received the welcome
-
-    s.join_room "#test1"
-    # as soon as the end of MOTD is received, the IrcServer will consider itself
-    # connected and try to join the rooms.
-    s.should_receive(:send_data).with("JOIN #test1\r\n")
-    s.should_receive(:send_data).with("JOIN #test2\r\n")
-
-    send_welcome(s) { s.join_room "#test2" }
-
-    s.connected?.should be_true
+    s.should_receive(:send_msg).with("USER speccer localhost blah :speccer")
+    s.should_receive(:send_msg).with("NICK speccer")
+    s.should_receive(:send_msg).with("AWAY :Away")
+    s.connection_established
   end
 
   it "should pong" do
     s = connected_server
-    s.should_receive(:send_data).with("PONG speccer!tkellem :HAI\r\n")
-    s.receive_line(":speccer!test@host ping :HAI")
+    s.should_receive(:send_msg).with("PONG tkellem!tkellem :HAI")
+    s.server_msg(IrcMessage.parse(":speccer!test@host ping :HAI"))
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,5 +4,8 @@ $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'tkellem'
 require 'rspec'
 
+ActiveRecord::Base.establish_connection(:adapter => 'sqlite3', :database => ':memory:')
+ActiveRecord::Migrator.migrate(File.expand_path("../../lib/tkellem/migrations", __FILE__), nil)
+
 RSpec.configure do |config|
 end
