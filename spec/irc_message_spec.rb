cc0f1377f401d0ae1811ada50e5b0c0504139e61
Brian Palmer <brianp@instructure.com>
2013-06-26 20:02:04 -0600

    expand the irc_message test suite

--- spec/irc_message_spec.rb
+++ spec/irc_message_spec.rb
@@ -1,5 +1,6 @@
 require 'spec_helper'
 require 'tkellem/irc_message'
+require 'time'
 
 include Tkellem
 
@@ -41,11 +42,18 @@ end
 describe IrcMessage, "#with_timestamp" do
   it "should prefix a timestamp to the last arg" do
     line = IrcMessage.parse(":some_long_prefix COMMAND first second :long arg here")
-    require 'time'
     timestamp = Time.parse("Thu Nov 29 14:33:20 2001")
     ts_line = line.with_timestamp(timestamp)
     ts_line.should be_a(IrcMessage)
     ts_line.to_s.should == ":some_long_prefix COMMAND first second :2001-11-29 14:33:20> long arg here"
+  end
+
+  it "should not prefix the date if the message is < 24 hours old" do
+    line = IrcMessage.parse(":some_long_prefix COMMAND first second :long arg here")
+    timestamp = 3.hours.ago
+    ts_line = line.with_timestamp(timestamp)
+    ts_line.should be_a(IrcMessage)
+    ts_line.to_s.should == ":some_long_prefix COMMAND first second :#{timestamp.strftime("%H:%M:%S")}> long arg here"
   end
 end
 
@@ -58,6 +66,22 @@ describe IrcMessage do
     line2 = IrcMessage.parse("TEST one two :three")
     line2.args.should == %w(one two three)
     line2.args.last.should == "three"
+  end
+
+  describe ".parse_client_command" do
+    it "should return nil for non-client commands" do
+      IrcMessage.parse_client_command("PRIVMSG a :b c d").should == nil
+    end
+
+    it "should turn /msg into a PRIVMSG command" do
+      IrcMessage.parse_client_command("/msg brian hai there").should ==
+        IrcMessage.new(nil, "PRIVMSG", ["brian", "hai there"])
+    end
+
+    it "should treat other commands literally" do
+      IrcMessage.parse_client_command("/join #tkellem").should ==
+        IrcMessage.new(nil, "JOIN", ["#tkellem"])
+    end
   end
 end
 
